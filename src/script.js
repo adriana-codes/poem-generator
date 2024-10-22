1c2b47fa85acd1677d107934f6328903c749b413
adriana-codes <helloadrianadiaz@gmail.com>
2024-10-22 16:15:27 +0200

    AI - JS integrated

---
+++ src/script.js
@@ -0,0 +1,23 @@
+function displaySong(response) {
+  new Typewriter("#salsa-song", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+function generateSong(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let key = "0be192793f55aa475o5602t2cabd6e24";
+  let prompt = `User Instructions: Generate a song about feeling ${instructionsInput.value}`;
+  let context =
+    "You are a salsa singer and songwriter who can come up with incredibly good lyrics from just one feeling. Your mission is to only generate a song in basic HTML following the user instructions in a <p> and separate each line with <br />. No need to add a song title.";
+  let urlApi = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(urlApi).then(displaySong);
+}
+
+let songElement = document.querySelector("#song-generator");
+songElement.addEventListener("submit", generateSong);
